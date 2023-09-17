3c9d7c1437648d7866bb3bc831e667059fe5bbdf
vinicius-g-alves <viniss201@gmail.com>
2023-09-17 18:47:45 -0300

    Add: styling in countries page

--- CountriesPage.jsx
+++ CountriesPage.jsx
@@ -1,37 +1,63 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { FlatList, Image, SafeAreaView, Text} from 'react-native'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FlatList, Image, SafeAreaView, Text, View } from "react-native";
 
 const CountriesPage = () => {
-  const [countriesInfo, setCountriesInfo] = useState([])
+  const [countriesInfo, setCountriesInfo] = useState([]);
 
   useEffect(() => {
     const getCountriesInfo = async () => {
       try {
-        const { data } = await axios.get(`https://restcountries.com/v3.1/all`)
-        setCountriesInfo(data)
-        data.forEach((it) => console.log(it.flags.svg))
+        const { data } = await axios.get(`https://restcountries.com/v3.1/all`);
+        setCountriesInfo(data);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
-    getCountriesInfo()
-  }, [])
+    };
+    getCountriesInfo();
+  }, []);
 
   return (
-    <SafeAreaView styles={{ justifyContent: 'center' }}>
+    <SafeAreaView>
+      <Text
+        style={{
+          marginTop: "15%",
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: 20,
+        }}
+      >
+        Selecione um país
+      </Text>
       <FlatList
+        keyExtractor={(item) => item.official}
+        numColumns={3}
+        style={{ marginTop: 20 }}
         data={countriesInfo}
-        renderItem={({ item }) => (
-        //   <Image
-        //     style={{ width: '10%', height: '5%' }}
-        //     source={{ uri: `${item.flags.png}` }}
-        //   />
-        <Text>aaaaaaaa</Text>
-        )}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                alignItems: "center",
+                backgroundColor: "lightgray",
+                flexGrow: 1,
+                margin: 4,
+                padding: 20,
+              }}
+            >
+              <Image
+                style={{
+                  width: 40,
+                  height: 40,
+                }}
+                source={{ uri: `${item.flags.png}` }}
+              />
+            </View>
+          );
+        }}
       />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default CountriesPage
+export default CountriesPage;
