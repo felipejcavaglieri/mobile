5a7cf1ca8b9133e6877d5114487d408bcbfb6f22
vinicius-g-alves <viniss201@gmail.com>
2023-09-17 17:16:52 -0300

    Add: login styles

--- Login.jsx
+++ Login.jsx
@@ -1,43 +1,71 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
-} from 'react-native'
+  View,
+} from "react-native";
 
 const Login = () => {
-  const [login, setLogin] = useState('')
-  const [password, setPassword] = useState('')
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput
-        onChangeText={setLogin}
-        value={login}
-        placeholder={'Usuário'}
-      />
-      <TextInput
-        onChangeText={setPassword}
-        value={password}
-        placeholder={'Senha'}
-        secureTextEntry={true}
-      />
-      <TouchableOpacity disabled={!login || !password}>
-        <Text>Login</Text>
-      </TouchableOpacity>
+    <SafeAreaView>
+      <Text style={styles.welcome}>Seja bem vindo!</Text>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setLogin}
+          value={login}
+          placeholder={"Usuário"}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          placeholder={"Senha"}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          style={styles.loginButton}
+          disabled={!login || !password}
+        >
+          <Text style={{ textAlign: "center" }}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "gray",
+    padding: 24,
+    borderRadius: 10,
+    margin: 24,
   },
-})
+  welcome: {
+    color: "black",
+    fontWeight: "bold",
+    marginTop: "50%",
+    fontSize: 20,
+    textAlign: "center",
+  },
+  input: {
+    padding: 8,
+    backgroundColor: "lightgray",
+    borderRadius: 10,
+    marginBottom: 24,
+  },
+  loginButton: {
+    backgroundColor: "lightblue",
+    padding: 8,
+    borderRadius: 10,
+    marginTop: "10%",
+  },
+});
 
-export default Login
+export default Login;
